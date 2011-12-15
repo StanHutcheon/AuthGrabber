4dc1b773c9958d65f559d1662790b513d45fec3a
Stan Hutcheon <stanley.h@hotmail.co.uk>
2011-12-15 23:27:58 +0000

    first commit

---
+++ scripts/certs.sh
@@ -0,0 +1,36 @@
+#read -p "Please enter here the domain or IP address of your Siri server (thing.something.com or 12.34.56.78): " SERVER
+read -p "Now enter your name: " NAME
+read -p "Now enter your email: " EMAIL
+read -p "When it asks for a password, just enter 'pass' for all of them (no quotations) Press enter to continue..."
+openssl genrsa -out ca.key 1024
+openssl req -new -x509 -days 365 -key ca.key -out ca.crt << EOF
+US
+California
+Cupertino
+$NAME, Inc.
+guzzoni.apple.com
+guzzoni.apple.com
+$EMAIL
+
+
+
+EOF
+openssl genrsa -des3 -out server.key 4096
+openssl req -new -key server.key -out server.csr << EOF
+US
+California
+Cupertino
+$NAME, Inc.
+guzzoni.apple.com
+guzzoni.apple.com
+$EMAIL
+
+
+
+EOF
+openssl x509 -req -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.passless.crt
+openssl rsa -in server.key -out server.key.insecure
+mv server.key server.key.secure
+mv server.key.insecure server.passless.key
+echo "Created."
+echo "Install ca.crt to your device by emailing it to yourself or putting it up on a web server or something."
---
+++ scripts/dependencies.sh
@@ -0,0 +1,16 @@
+echo "refreshing apt (just in case)..."
+sudo apt-get update
+echo "installing required stuff etc..."
+sudo apt-get install dnsmasq ruby build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
+echo "Installing dnsmasq settings..."
+sudo cp ./files/dnsmasq.conf /etc/dnsmasq.conf
+echo "Installing RVM..."
+bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
+echo "Installing ruby 1.9.3... [this takes bloody ages :p]"
+rvmsudo rvm install 1.9.3
+rvm use 1.9.3 --default
+echo "Installing required gems..."
+rvmsudo gem install eventmachine CFPropertyList httparty json uuidtools
+echo "done."
