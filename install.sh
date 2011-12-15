bb946b55db6accf1245a7b99adb436834ffccd67
Stan Hutcheon <stanley.h@hotmail.co.uk>
2011-12-15 23:39:08 +0000

    forgot install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo "This installs everything you need to get AuthGrabber working."
+echo "I will now begin..."
+sudo ./scripts/dependencies.sh
+sudo ./scripts/certs.sh
+echo "Everything is done. when you're ready, just type sudo ./cache.sh in terminal :)"
