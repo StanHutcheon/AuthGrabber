81a0fbdee16a97bd7ec5fc9154891a3c9b4d3625
Stan Hutcheon <stanley.h@hotmail.co.uk>
2011-12-16 19:47:03 +0000

    updated readme and fixed permissions

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 echo "This installs everything you need to get AuthGrabber working."
 echo "I will now begin..."
+sudo chmod -R 770 ./
 sudo ./scripts/dependencies.sh
 sudo ./scripts/certs.sh
 echo "Everything is done. when you're ready, just type sudo ./cache.sh in terminal :)"
