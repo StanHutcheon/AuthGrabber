92e93c8f4d23f3f5d6f63721ee3db35a2a03fed7
Stanley Hutcheon <stanley.h@hotmail.co.uk>
2011-12-23 22:07:31 +0000

    updated cache.sh to use rvmsudo

--- cache.sh
+++ cache.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 
-sudo ruby ./authGrabber.rb > auths/acehosts0.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts0.txt
 echo Request cached, writing to auths/acehosts0.txt
-sudo ruby ./authGrabber.rb > auths/acehosts1.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts1.txt
 echo Request cached, writing to auths/acehosts1.txt
-sudo ruby ./authGrabber.rb > auths/acehosts2.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts2.txt
 echo Request cached, writing to auths/acehosts2.txt
-sudo ruby ./authGrabber.rb > auths/acehosts3.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts3.txt
 echo Request cached, writing to auths/acehosts3.txt
-sudo ruby ./authGrabber.rb > auths/acehosts4.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts4.txt
 echo Request cached, writing to auths/acehosts4.txt
-sudo ruby ./authGrabber.rb > auths/acehosts5.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts5.txt
 echo Request cached, writing to auths/acehosts5.txt
 
-sudo ruby ./authGrabber.rb > auths/acehosts6.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts6.txt
 echo Request cached, writing to auths/acehosts6.txt
-sudo ruby ./authGrabber.rb > auths/acehosts7.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts7.txt
 echo Request cached, writing to auths/acehosts7.txt
-sudo ruby ./authGrabber.rb > auths/acehosts8.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts8.txt
 echo Request cached, writing to auths/acehosts8.txt
-sudo ruby ./authGrabber.rb > auths/acehosts9.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts9.txt
 echo Request cached, writing to auths/acehosts9.txt
 
-sudo ruby ./authGrabber.rb > auths/acehosts10.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts10.txt
 echo Request cached, writing to auths/acehosts10.txt
-sudo ruby ./authGrabber.rb > auths/acehosts11.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts11.txt
 echo Request cached, writing to auths/acehosts11.txt
-sudo ruby ./authGrabber.rb > auths/acehosts12.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts12.txt
 echo Request cached, writing to auths/acehosts12.txt
-sudo ruby ./authGrabber.rb > auths/acehosts13.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts13.txt
 echo Request cached, writing to auths/acehosts13.txt
-sudo ruby ./authGrabber.rb > auths/acehosts14.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts14.txt
 echo Request cached, writing to auths/acehosts14.txt
-sudo ruby ./authGrabber.rb > auths/acehosts15.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts15.txt
 echo Request cached, writing to auths/acehosts15.txt
 
-sudo ruby ./authGrabber.rb > auths/acehosts16.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts16.txt
 echo Request cached, writing to auths/acehosts16.txt
-sudo ruby ./authGrabber.rb > auths/acehosts17.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts17.txt
 echo Request cached, writing to auths/acehosts17.txt
-sudo ruby ./authGrabber.rb > auths/acehosts18.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts18.txt
 echo Request cached, writing to auths/acehosts18.txt
-sudo ruby ./authGrabber.rb > auths/acehosts19.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts19.txt
 echo Request cached, writing to auths/acehosts19.txt
-sudo ruby ./authGrabber.rb > auths/acehosts20.txt
+rvmsudo ruby ./authGrabber.rb > auths/acehosts20.txt
 echo Request cached, writing to auths/acehosts20.txt
